eb345ddaf089a6e0cf079aa06480c221f7720f51
serdaraydin <serdara2000@gmail.com>
2024-04-19 16:42:13 +0300

    NetxtAuth google login ekledik.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { signIn, useSession, signOut } from "next-auth/react";
 
 export default function Header() {
+
+  const { data: session } = useSession();
+  console.log(session);
+  
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
@@ -31,7 +38,19 @@ export default function Header() {
         />
 
         {/* menu item  */}
-        <button className="text-sm font-semibold text-blue-500">Log In</button>
+        {session ? (
+          <img src={session.user.image} alt={session.user.className} className="h-10 w-10 rounded-full cursor-pointer" onClick={signOut} />
+
+        ) : (
+
+        <button
+          onClick={signIn}
+          className="text-sm font-semibold text-blue-500"
+        >
+          Log In
+        </button>
+        )
+        }
       </div>
     </div>
   );
